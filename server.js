acfa8b69b5759a07db54c5bb9c1d3d078b85d4b4
Oleksandr Kropivnyi <kropivnyy@gmail.com>
2025-04-28 18:29:17 +0300

    added cors and rate-limit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const connectDB = require('./config/db');
@@ -10,8 +12,29 @@ dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  }),
+);
+
 // Middleware
 app.use(express.json({ limit: '10kb' }));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    success: false,
+    error: {
+      code: 429,
+      message: 'Забагато запитів в вашого IP. Спробуйте пізніше.',
+    },
+  },
+});
+
+app.use(limiter);
 
 // Routes
 app.use('/api/auth', authRoutes);
