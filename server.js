80fb51254161481e587d9f9fd18973b06e731b24
Oleksandr Kropivnyi <kropivnyy@gmail.com>
2025-06-04 11:43:14 +0300

    returned cors origins

--- server.js
+++ server.js
@@ -15,18 +15,18 @@ const app = express();
 
 // Middlewares
 const corsOptions = {
-  // origin: [
-  //   'http://localhost:5173',
-  //   'https://hansen-admin.netlify.app',
-  //   'https://hansen-fe.netlify.app',
-  // ],
-  origin: '*',
+  origin: [
+    'http://localhost:5173',
+    'https://hansen-admin.netlify.app',
+    'https://hansen-fe.netlify.app',
+  ],
+  // origin: '*',
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 };
 app.use(cors(corsOptions));
-// app.options('/*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 app.use(express.json({ limit: '10kb' }));
 
